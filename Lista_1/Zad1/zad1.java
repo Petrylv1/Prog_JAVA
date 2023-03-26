a8e2c166238b51d780837c953ed5fc1ce81a2589
Petryl <62471248+Petrylv1@users.noreply.github.com>
2023-03-27 01:15:34 +0200

    Add files via upload

--- Lista_1/Zad1/zad1.java
+++ Lista_1/Zad1/zad1.java
@@ -3,20 +3,36 @@ import java.util.Scanner;
 public class zad1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
+        System.out.print("Podaj ilosc przedmiotow: ");
+        int liczbaPrzedmiotow = scanner.nextInt();
+
         System.out.print("Podaj ilosc ocen do wprowadzenia: ");
         int iloscOcen = scanner.nextInt();
-        
+
+        int[][] oceny = new int[liczbaPrzedmiotow][iloscOcen];
         int sumaOcen = 0;
-        for (int i = 1; i <= iloscOcen; i++) {
-            System.out.print("Podaj ocene z przedmiotu " + i + ": ");
-            int ocena = scanner.nextInt();
-            sumaOcen += ocena;
+
+        for (int i = 0; i < liczbaPrzedmiotow; i++) {
+            for (int j = 0; j < iloscOcen; j++) {
+                int ocena;
+                do {
+                    System.out.print("Podaj ocene " + (j+1) + " z przedmiotu " + (i+1) + ": ");
+                    ocena = scanner.nextInt();
+                    if (ocena < 2 || ocena > 5) {
+                        System.out.println("Ocena musi byc w skali 2 - 5.");
+                    }
+                } while (ocena < 2 || ocena > 5);
+
+                oceny[i][j] = ocena;
+                sumaOcen += oceny[i][j];
+            }
         }
-        
-        double srednia = (double) sumaOcen / iloscOcen;
+
+        int iloscWszystkichOcen = iloscOcen * liczbaPrzedmiotow;
+        double srednia = (double) sumaOcen / iloscWszystkichOcen;
         System.out.println("Twoja srednia wynosi: " + srednia);
-        
+
         if (srednia > 4.1) {
             System.out.println("Kwalifikujesz sie do stypendium.");
         } else {
